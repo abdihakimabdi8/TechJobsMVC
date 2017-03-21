563357bdd904e7a612f960cefb711efa61d6b8c1
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-03-21 18:42:41 -0500

    Search All functionality, case insensitivity implemented.

---
+++ src/TechJobsMVC/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            return View();
+        }
+        public IActionResult Jobs(string column, string searchTerm)
+        {
+            if (column.Equals("all"))
+            {
+             
+                List<Dictionary<string, string>> jobs = JobData.FindByValue( searchTerm);
+                ViewBag.title = "All Jobs";
+                ViewBag.searchTerm = searchTerm;
+                ViewBag.jobs = jobs;
+                return View("index");
+            }
+            else
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(column, searchTerm);
+                ViewBag.title = "Jobs with " + ViewBag.columns[column] + ": " + searchTerm;
+                ViewBag.jobs = jobs;
+
+                return View();
+            }
+        }
+    }
+}
