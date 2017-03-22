a72b3e0cb024cb7de792687d9e1587f9aa93be9b
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-03-22 18:40:46 -0500

    Submittable version

--- src/TechJobsMVC/Controllers/SearchController.cs
+++ src/TechJobsMVC/Controllers/SearchController.cs
@@ -18,22 +18,25 @@ namespace TechJobs.Controllers
         [HttpPost]
 
         [Route("/Search")]
-        public IActionResult Results(string column, string searchTerm)
+        public IActionResult Results(string searchType, string searchTerm)
         {
             ViewBag.columns = ListController.columnChoices;
-            if (column.Equals("all"))
+            if (searchType.Equals("all"))
 
             {
-             
+                ViewBag.columns = ListController.columnChoices;
                 List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
                 ViewBag.title = "All Jobs";
+                ViewBag.searchTerm = ViewBag.searchTerm;
                 ViewBag.jobs = jobs;
             
             }
             else
             {
-                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(column, searchTerm);
-                ViewBag.title = "Jobs with " + ViewBag.columns[column] + ": " + searchTerm;
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Jobs with " + ViewBag.columns[searchType] + ": " + searchTerm;
+                ViewBag.column = ViewBag.column;
+                ViewBag.searchTerm = ViewBag.searchTerm;
                 ViewBag.jobs = jobs;
 
                 
